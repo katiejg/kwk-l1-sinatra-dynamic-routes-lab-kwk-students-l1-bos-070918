7c5486a77b70d6fbf6f19b541b7c47f5e34030d8
katiejg <jng.katie@gmail.com>
2018-07-29 22:31:55 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -19,8 +19,8 @@ class App < Sinatra::Base
   "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}." 
   end
   get '/:operation/:number1/:number2' do
-    num1 = params[:number1]
-    num2 = params[:number2]
+    num1 = params[:number1].to_i
+    num2 = params[:number2].to_i
     if params[:operation] == "add"
       answer = num1 + num2
       elsif params[:operation] == "subtract"
