80f19a376fdee10a237ba6f3c4ae61be0e1108dd
katiejg <jng.katie@gmail.com>
2018-07-29 22:06:35 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,5 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
-
+  get '/reversename/:name'
+    puts "What name would you like to reverse?"
+    name = gets.chomp
+    
 end
